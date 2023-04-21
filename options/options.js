99c4ac6a9a7b1e76e476d148529a3acea9608cc2
Crowdbotics <team@crowdbotics.com>
2023-04-21 22:55:26 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "fitai-40108",
+  url: "https://fitai-40108.botics.co",
+  api: "https://fitai-40108.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
